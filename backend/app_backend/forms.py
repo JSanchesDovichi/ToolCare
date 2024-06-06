4ba97cd884fc992354b59950b3f321d8ca67d5e5
João Pedro Sanches Dovichi <jsanchesdovichi@gmail.com>
2024-06-06 00:05:25 -0300

    Big Bang

--- backend/app_backend/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from.models import Ferramenta
-
-class FerramentaForm(forms.ModelForm):
-    class Meta:
-        model = Ferramenta
-        fields = ['nome', 'numSerie', 'descricao', 'imgFerramenta', 'dataAquisicao', 'status']
